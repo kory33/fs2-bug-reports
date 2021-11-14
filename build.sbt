a3b5cd1353cf4b740f65f6fa43143c942d3da9bc
Kory <korygm33@gmail.com>
2021-11-14 23:04:28 +0000

    add minimal example

---
+++ build.sbt
@@ -0,0 +1,4 @@
+scalaVersion := "3.1.0"
+libraryDependencies ++= Seq(
+  "co.fs2" %% "fs2-io" % "3.2.2"
+)
---
+++ src/main/scala/io/github/kory33/bugreports/fs2/io/net/Test.scala
@@ -0,0 +1,41 @@
+package io.github.kory33.bugreports.fs2.io.net
+
+object SocketImplTest {
+  @main def main: Unit = {
+    import cats.Monad
+    import cats.effect.{IO, Resource, Spawn}
+    import com.comcast.ip4s.SocketAddress
+    import fs2.io.net.Network
+
+    import cats.implicits.given
+    import cats.effect.unsafe.implicits.global
+
+    val portNumber = 31629
+    val serverAddress = SocketAddress.fromString(s"localhost:$portNumber").get
+    val clientResource: Resource[IO, fs2.io.net.Socket[IO]] =
+      for {
+        // we prepare a tcp server which returns 0 for every read from a client
+        _ <- Spawn[IO].background {
+          Network[IO]
+            .server(address = Some(serverAddress.host), port = Some(serverAddress.port))
+            .evalMap(socket =>
+              Monad[IO].whileM_(socket.isOpen)(socket.write(fs2.Chunk[Byte](0)))
+            )
+            .compile
+            .drain
+        }
+        socket <- Network[IO].client(serverAddress)
+      } yield socket
+
+    val program: IO[Unit] =
+      clientResource.use { client =>
+        Monad[IO].foreverM {
+          client.read(1024).start.flatMap { fiber =>
+            fiber.cancel >> client.read(1024) >> fiber.join
+          }
+        }
+      }.void
+
+    program.unsafeRunSync()
+  }
+}
